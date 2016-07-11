903c7321bbd33926acdf3412553fb50aea4f01a5
= <mail@matbee.com>
2016-07-11 00:21:13 -0400

    .

--- examples_linux/test.cpp
+++ examples_linux/test.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv){
 	radio.openWritingPipe(pipes[radioNumber]);
 
 	for (uint8_t i = 0; i < 6; i++) {
-		if (i != radioNumber) {
+		if ((int)i != (int)radioNumber) {
 			std::ostringstream oss;
 			oss << "..." << (int)i;
 			std::cout << oss.str();
