b6a01965d1ddc38bbabcd3e7c9418624000a7993
= <mail@matbee.com>
2016-07-11 00:35:22 -0400

    .

--- examples_linux/test.cpp
+++ examples_linux/test.cpp
@@ -31,8 +31,8 @@ int main(int argc, char** argv){
 	radio.openWritingPipe(pipes[radioNumber]);
 
 	for (int i = 0; i < 6; i++) {
-		cout << radioNumber << "\n";
 		if (i != radioNumber) {
+			cout << i << "\n";
     		radio.openReadingPipe(i,pipes[i]);
 		}
 	}
